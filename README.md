68cd8cc1a74567b322399247e5054db4e86a0a00
Wenbo <wenbo@sparkle.im>
2024-01-15 23:22:40 +0800

    Wrapped with Tauri (#1)
    * feat: wrapped in Tauri starter template, need fix some problem
    
    * feat: seems background thread is ok
    
    * feat: add an simplist ui interface
    
    * fix: build errors
    
    * chores: resolve build errors

--- README.md
+++ README.md
@@ -1,29 +1,11 @@
-# MiBand Heart Rate Demo
+# Tauri + React
 
-A Demo of reading "Shear heart rate data" of Xiaomi Smart Band 6. Enable the option in official App is required.
+This template should help get you started developing with Tauri and React in Vite.
 
-接收小米手环6 “运动心率广播” Demo，测试前需要在小米运动健康App中开启手环的该选项。
+## Recommended IDE Setup
 
-欢迎二次开发。
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
 
-## Supported Platform
+### How to resolve build error?
 
-I use `bluest` crate. I copy its words below.
-
-> Bluest is a cross-platform Bluetooth Low Energy (BLE) library for Rust. It currently supports Windows (version 10 and later), MacOS/iOS, and Linux. Android support is planned.
-
-So it supported:
-
-- Windows 10
-- MacOS/iOS
-- Linux
-
-## Supported MiBands
-
-MiBand4, 5, 6, 7 小米手环4、5、6、7
-
-Tested on MiBand6/NFC.
-
-## Screenshot
-
-![Alt text](doc/screenshot.png)
+Github [link](https://github.com/tauri-apps/tauri/issues/7338#issuecomment-1622960138)
---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import heartPulseSvg from "./assets/HeartPulse20Regular.svg";
+import "./App.css";
+
+// 自定义 hook
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
+
+function App() {
+  const [heartbeat, setHeartbeat] = useState("")
+
+  useInterval(async () => { // <= useInterval hook
+    setHeartbeat(await invoke("heartbeat"));
+  }, 1000);
+
+  return (
+    <div className="container">
+      {
+        heartbeat && heartbeat !== '255' ? (
+          <div className="box">
+            <i className="icon ico-heartbeat">
+              <img src={heartPulseSvg} />
+            </i>
+            <span>{heartbeat}</span>
+          </div>
+        ) : (
+          <div className="box error">Receiver is not running, please check or reconnect.</div>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
